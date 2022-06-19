6a69a66d75f7078c8b500b465df599f5e2ddab3f
anish0803 <singhanish124@gmail.com>
2022-06-19 16:09:45 +0530

    updated q-3

--- first_last_digit.js
+++ first_last_digit.js
@@ -2,10 +2,24 @@
 // 3. Write a program to read the first and last digit of a number
 
 
-function firstAndLast(num){
-    let a = Math.floor(num/100000);
-      let b = num%10;
-      return(a+" "+b);
+// function firstAndLast(num){
+//     let a = Math.floor(num/100000);
+//       let b = num%10;
+//       return(a+" "+b);
+//     }
+//     console.log(firstAndLast(123789));
+//     exports.firstAndLast = firstAndLast;
+
+
+
+
+    // --------------While loop----------
+    
+    let Number = parseInt(process.argv[2]); 
+    let firstdigit, lastdigit;
+    lastdigit= Number % 10;
+    while(Number>=10){
+      firstdigit=Math.floor(Number/10);
+      Number=Number/10;
     }
-    console.log(firstAndLast(123789));
-    exports.firstAndLast = firstAndLast;
+    console.log(firstdigit,lastdigit);
